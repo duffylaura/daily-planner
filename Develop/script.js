4093cf84f3f28beebf8b43cc5985842dea95fda2
Coffee Pawz <lauralynneduffy@gmail.com>
2022-11-03 19:30:29 -0400

    Function that changes the color of the time blocks based on if the task is past/present/future is operational.

--- Develop/script.js
+++ Develop/script.js
@@ -6,22 +6,14 @@ function displayTime () {
 };
 setInterval (displayTime, 1000); 
 
-// time colors// 
-
-//Save reference to DOM elements
-// var nineEl = $('#9-color'); var tenEl = $('#10-color'); var elevenEl = $('#11-color'); var tweleveEl = $('#12-color'); var thirteenEl = $('#13-color'); var fourteenEl = $('#14-color'); var fifteenEl = $('#15-color'); var sixteenEl = $('#16-color'); var seventeenEl = $('#17-color'); 
-
-
-// Function to that determines colors based on hour
-
-// PRESENT = #A0E1F0  // PAST = #F0B9AD  // FUTURE = #F0D595
+// Function to that determines colors based on hour. Colors used: PRESENT = #A0E1F0  // PAST = #F0B9AD  // FUTURE = #F0D595
 
 function timeColors () {
 
     var now = moment().format('H');   
 
-    if (now == 19) { 
-        document.getElementById("9-color").style.backgroundColor = '#A0E1F0';
+    if (now == 9) { 
+        document.getElementById("9-color").style.backgroundColor =  '#A0E1F0'; // Now
         document.getElementById("10-color").style.backgroundColor = '#F0D595';
         document.getElementById("11-color").style.backgroundColor = '#F0D595';
         document.getElementById("12-color").style.backgroundColor = '#F0D595';
@@ -31,11 +23,121 @@ function timeColors () {
         document.getElementById("16-color").style.backgroundColor = '#F0D595';
         document.getElementById("17-color").style.backgroundColor = '#F0D595';
     }
+    else if (now == 10) {
+        document.getElementById("9-color").style.backgroundColor  = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#A0E1F0'; // Now
+        document.getElementById("11-color").style.backgroundColor = '#F0D595';
+        document.getElementById("12-color").style.backgroundColor = '#F0D595';
+        document.getElementById("13-color").style.backgroundColor = '#F0D595';
+        document.getElementById("14-color").style.backgroundColor = '#F0D595';
+        document.getElementById("15-color").style.backgroundColor = '#F0D595';
+        document.getElementById("16-color").style.backgroundColor = '#F0D595';
+        document.getElementById("17-color").style.backgroundColor = '#F0D595';
+    }
+    else if (now == 11) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#A0E1F0'; // Now
+        document.getElementById("12-color").style.backgroundColor = '#F0D595';
+        document.getElementById("13-color").style.backgroundColor = '#F0D595';
+        document.getElementById("14-color").style.backgroundColor = '#F0D595';
+        document.getElementById("15-color").style.backgroundColor = '#F0D595';
+        document.getElementById("16-color").style.backgroundColor = '#F0D595';
+        document.getElementById("17-color").style.backgroundColor = '#F0D595';      
+    }
+    else if (now == 12) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("12-color").style.backgroundColor = '#A0E1F0'; // Now
+        document.getElementById("13-color").style.backgroundColor = '#F0D595';
+        document.getElementById("14-color").style.backgroundColor = '#F0D595';
+        document.getElementById("15-color").style.backgroundColor = '#F0D595';
+        document.getElementById("16-color").style.backgroundColor = '#F0D595';
+        document.getElementById("17-color").style.backgroundColor = '#F0D595';          
+    }
+    else if (now == 13) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("12-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("13-color").style.backgroundColor = '#A0E1F0'; // Now
+        document.getElementById("14-color").style.backgroundColor = '#F0D595';
+        document.getElementById("15-color").style.backgroundColor = '#F0D595';
+        document.getElementById("16-color").style.backgroundColor = '#F0D595';
+        document.getElementById("17-color").style.backgroundColor = '#F0D595';    
+    }
+    else if (now == 14) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("12-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("13-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("14-color").style.backgroundColor = '#A0E1F0'; // Now
+        document.getElementById("15-color").style.backgroundColor = '#F0D595';
+        document.getElementById("16-color").style.backgroundColor = '#F0D595';
+        document.getElementById("17-color").style.backgroundColor = '#F0D595';    
+    }
+    else if (now == 15) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("12-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("13-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("14-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("15-color").style.backgroundColor = '#A0E1F0'; // Now
+        document.getElementById("16-color").style.backgroundColor = '#F0D595';
+        document.getElementById("17-color").style.backgroundColor = '#F0D595';    
+    }
+    else if (now == 16) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("12-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("13-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("14-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("15-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("16-color").style.backgroundColor = '#A0E1F0'; // Now
+        document.getElementById("17-color").style.backgroundColor = '#F0D595';    
+    }
+    else if (now == 17) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("12-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("13-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("14-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("15-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("16-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("17-color").style.backgroundColor = '#A0E1F0'; // Now    
+    }
 
-        
-
-
-
-}
+    else if (now > 17 && now < 24) {
+        document.getElementById("9-color").style.backgroundColor = '#F0B9AD'; // all in the past
+        document.getElementById("10-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("11-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("12-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("13-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("14-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("15-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("16-color").style.backgroundColor = '#F0B9AD';
+        document.getElementById("17-color").style.backgroundColor = '#F0B9AD';   
+    } 
+    
+    else if (now > 0 && now < 9) {
+        document.getElementById("9-color").style.backgroundColor =  '#F0D595'; // all in the future 
+        document.getElementById("10-color").style.backgroundColor = '#F0D595';
+        document.getElementById("11-color").style.backgroundColor = '#F0D595';
+        document.getElementById("12-color").style.backgroundColor = '#F0D595';
+        document.getElementById("13-color").style.backgroundColor = '#F0D595';
+        document.getElementById("14-color").style.backgroundColor = '#F0D595';
+        document.getElementById("15-color").style.backgroundColor = '#F0D595';
+        document.getElementById("16-color").style.backgroundColor = '#F0D595';
+        document.getElementById("17-color").style.backgroundColor = '#F0D595'; 
+    }
+    
+    };
 
 timeColors();
+
+// 
